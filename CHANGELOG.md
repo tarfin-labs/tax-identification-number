e3ecf59881b312f2f9c382f3611f05e5c3e75573
Turan Karatuğ <tkaratug@hotmail.com.tr>
2021-09-03 11:18:08 +0300

    v1.0.1 changes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to `vkn-validation` will be documented in this file
 
-## 1.0.0 - 201X-XX-XX
+## 1.0.1 - 2021-09-03
+
+- Getters returned null if parsed response is empty on `Response` class.
+
+## 1.0.0 - 2021-07-27
 
 - initial release
--- src/Response.php
+++ src/Response.php
@@ -13,34 +13,34 @@ class Response
         $this->response = $this->parse($response);
     }
 
-    public function getStatus(): string
+    public function getStatus(): ?string
     {
-        return $this->response->durum;
+        return $this->response->durum ?? null;
     }
 
-    public function getTckn(): string
+    public function getTckn(): ?string
     {
-        return $this->response->tckn;
+        return $this->response->tckn ?? null;
     }
 
-    public function getStatusText(): string
+    public function getStatusText(): ?string
     {
-        return $this->response->durum_text;
+        return $this->response->durum_text ?? null;
     }
 
-    public function getTaxNumber(): string
+    public function getTaxNumber(): ?string
     {
-        return $this->response->vkn;
+        return $this->response->vkn ?? null;
     }
 
-    public function getTaxOfficeNumber(): string
+    public function getTaxOfficeNumber(): ?string
     {
-        return $this->response->vdkodu;
+        return $this->response->vdkodu ?? null;
     }
 
-    public function getCompanyTitle(): string
+    public function getCompanyTitle(): ?string
     {
-        return $this->response->unvan;
+        return $this->response->unvan ?? null;
     }
 
     public function isValid(): bool
